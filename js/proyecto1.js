6598bb36f215cdc117cd6d5de1fe87de86da8e14
Agustin Caceres <sebaxt10@outlook.com>
2022-07-04 09:45:32 -0300

    terminados las 5 actividades

--- js/proyecto1.js
+++ js/proyecto1.js
@@ -2,3 +2,7 @@ alert("un mensaje");
 document.write("<h1>Hola mundo</h1>");
 document.write("Resultado: "+(3 + 5));
 nombre = prompt("Hola nombreUsuario")
+numero1=parseInt(prompt("digite el numero 1:"));
+numero2=parseInt(prompt("digite el numero 2:"));
+total=(numero1+numero2);
+alert("el resultado de la suma es: " + total)
