f7a3990f5e7fb5e8003c41bec6a5f39814f8363d
Agustin Caceres <sebaxt10@outlook.com>
2022-07-04 10:15:07 -0300

    hechas las 5 actividades

--- js/proyecto1.js
+++ js/proyecto1.js
@@ -2,7 +2,6 @@ alert("un mensaje");
 document.write("<h1>Hola mundo</h1>");
 document.write("Resultado: "+(3 + 5));
 nombre = prompt("Hola nombreUsuario")
-numero1=parseInt(prompt("digite el numero 1:"));
-numero2=parseInt(prompt("digite el numero 2:"));
-total=(numero1+numero2);
-alert("el resultado de la suma es: " + total)
+let numero1 = parseInt(prompt("digite el numero 1:"));
+let numero2 = parseInt(prompt("digite el numero 2:"));
+document.write(" la suma es " + (numero1 + numero2))
